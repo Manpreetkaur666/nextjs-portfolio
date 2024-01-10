2c134565f5e0c32443f6e5d224e0f780c32e826e
Manpreetkaur666 <mk27982@gmail.com>
2024-01-10 13:22:56 -0500

    edit nav

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { navData } from "../data/navData";
 import Link from "next/link";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+  // useEffect(() => {
+  //   setShow(false);
+  // }, [show,setShow]);
+  
+  console.log(show);
   return (
     <div>
       <nav className="bg-transparent border-gray-200 dark:bg-gray-900 p-4">
@@ -17,6 +24,7 @@ const Navbar = () => {
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
+            onClick={() => setShow(!show)}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -36,14 +44,16 @@ const Navbar = () => {
             </svg>
           </button>
           <div
+            style={{ display: show ? "initial" : "none" }}
             className="hidden w-full md:block md:w-auto bg-transparent"
             id="navbar-default"
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {navData.map((item, index) => (
-                <li key={index} className="hover:text-yellow-color">
+                <li key={index} className="hover:text-yellow-color text-right">
                   <Link
                     href={item.link}
+                    onClick={() => setShow(false)}
                     className="hover:transition ease-in-out delay-150 hover:-translate-y-0.5 block py-2 px-3 text-white hover:text-yellow-color md:border-0 md:p-0 dark:text-white"
                   >
                     {item.name}
