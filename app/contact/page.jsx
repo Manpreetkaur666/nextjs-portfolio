7eebbafb8cf8efa750594a3052f35ee786966409
Manpreetkaur666 <mk27982@gmail.com>
2024-01-10 14:30:12 -0500

    add resume link

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -86,7 +86,7 @@ const Page = () => {
           </div>
           <div className="w-fit mx-auto my-8">
             <a
-              href="https://drive.google.com/file/d/1y4hwqhG3vLZUPxtal2ywLpICCwZ59qPF/view?usp=sharing"
+              href="https://drive.google.com/file/d/1lneIpenS8Bw4xhuH-vVfojAR39f-dWhi/view?usp=sharing"
               target="_blank"
               className="bg-main-light hover:bg-yellow-color text-main-color font-bold py-2 px-8 my-4 rounded"
             >
